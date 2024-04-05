6097af97bc7317b58d55b99822d429eea61b2ea6
Pavel Uvarov <pauknone@yahoo.com>
2024-04-05 19:11:19 +0300

    TASK-106 - improve DRY-appliance

--- src/main/java/net/webcumo/test/exercise106/violationsearchers/ManagersSalaryTooHighSearcher.java
+++ src/main/java/net/webcumo/test/exercise106/violationsearchers/ManagersSalaryTooHighSearcher.java
@@ -1,23 +1,14 @@
 package net.webcumo.test.exercise106.violationsearchers;
 
-import net.webcumo.test.exercise106.employee.EmployeeTreeElement;
+import net.webcumo.test.exercise106.employee.Employee;
 
 public class ManagersSalaryTooHighSearcher extends AbstractManagerViolationSearcher {
     static final String VIOLATION = "Manager %s has more than 150%% of it's subordinates average.%n";
 
     @Override
-    public void searchViolations(EmployeeTreeElement ceo) {
-        if (ceo.hasSubordinates()) {
-            searchViolationsOnManager(ceo);
+    void checkViolations(Employee manager, double averageSubordinatesSalary) {
+        if (manager.salary() > averageSubordinatesSalary * 1.5) {
+            System.out.printf(VIOLATION, manager);
         }
-    }
-
-    private void searchViolationsOnManager(EmployeeTreeElement manager) {
-        if (manager.getEmployee().salary() > calculateSubordinatesAverageSalary(manager) * 1.5) {
-            System.out.printf(VIOLATION, manager.getEmployee());
-        }
-        manager.getSubordinates().parallelStream()
-                .filter(EmployeeTreeElement::hasSubordinates)
-                .forEach(this::searchViolationsOnManager);
     }
 }
--- src/main/java/net/webcumo/test/exercise106/violationsearchers/ManagersSalaryTooLowSearcher.java
+++ src/main/java/net/webcumo/test/exercise106/violationsearchers/ManagersSalaryTooLowSearcher.java
@@ -1,24 +1,15 @@
 package net.webcumo.test.exercise106.violationsearchers;
 
-import net.webcumo.test.exercise106.employee.EmployeeTreeElement;
+import net.webcumo.test.exercise106.employee.Employee;
 
 public class ManagersSalaryTooLowSearcher extends AbstractManagerViolationSearcher {
     static final String VIOLATION = "Manager %s has lower than 120%% of it's subordinates average.%n";
 
     @Override
-    public void searchViolations(EmployeeTreeElement ceo) {
-        if (ceo.hasSubordinates()) {
-            searchViolationsOnManager(ceo);
+    void checkViolations(Employee manager, double averageSubordinatesSalary) {
+        if (manager.salary() < averageSubordinatesSalary * 1.2) {
+            System.out.printf(VIOLATION, manager);
         }
-    }
-
-    public void searchViolationsOnManager(EmployeeTreeElement manager) {
-        if (manager.getEmployee().salary() < calculateSubordinatesAverageSalary(manager) * 1.2) {
-            System.out.printf(VIOLATION, manager.getEmployee());
-        }
-        manager.getSubordinates().parallelStream()
-                .filter(EmployeeTreeElement::hasSubordinates)
-                .forEach(this::searchViolationsOnManager);
     }
 
 }
